1f903fe348bc6b3525e74a6aaed941e1b0a91015
afalkner740 <afalknerpersonal@gmail.com>
2025-09-08 03:19:20 +0000

    Done with lab.

--- FutureTime.py
+++ FutureTime.py
@@ -1,25 +1,25 @@
 #FutureTime.py
-#Name:
-#Date:
-#Assignment:
+#Name:Alexa Falkner
+#Date:9/7/2025
+#Assignment:Lab 2
 
 # datetime will allow us to access the system date and time.
-import datetime
-
 def main():
-  #getting current time from system, storing to variable
+  import datetime
   now = datetime.datetime.now()
-  currentHour = now.hour
+  currentHour = (now.hour - 5) % 24
   currentMinute = now.minute
-
-  print (currentHour, currentMinute) #this is just for checking, we should delete it later
-
+  print(currentHour , currentMinute)
+ #this is just for checking, we should delete it later
   #TODO:
   #Ask user for hours
+  hours  = int(input("What hours from 0-24? "))
   #Ask user for minutes
-
+  minutes = int (input ("What minutes from 0-60? "))
   #Calculate the time after the user-supplied time has passed.
-
+  futurehours = (currentHour + hours) % 24
+  futureMins = (currentMinute + minutes) % 60
+  print(futurehours ,futureMins)
   #Do not use any if statements in calculating the time.
 
   #Output the future time in standard format "HH:MM"
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,7 +1,7 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
+#Name:Alexa Falkner
+#Date:9/7/2025
+#Assignment:Lab 2
 
 #We will need random for this program, import to use this package.
 import random
@@ -10,9 +10,15 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
-r = random.random() * 5
+question = input ("What is your question? ")
+answer = ["Concentrate and ask again.", "Yes. Wait actually NO.","Don't count on it.","Very Doubtful.",
+          "Absolutely NOT.","Uhh yes!","I don't care."]
+length = len(answer)
+r = random.random() * length
+
 r = int(r) 
-response = answers [r]
+response = answer [r]
+print(response)
   #Answer question randomly with one of the options from your earlier list.
 
 
